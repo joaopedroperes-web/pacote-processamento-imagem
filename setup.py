737cefb54eb3c3d32f4f395bce98cd60e92ba28a
joao_pedro_ps <jpperesgl@gmail.com>
2022-10-13 15:15:24 -0300

    codigoP

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="pacote_processamento_img",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="joao_pedro_ps",
+    author_email="jpperes@gmail.com",
+    description="projeto_desenvolvido_durante_aoda_de_processamento_de_imagens",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/joaopedroperes-web/pacote-processamento-imagem",
     packages=find_packages(),
     install_requires=requirements,
-    python_requires='>=3.8',
+    python_requires="Python>=3.8"
 )
